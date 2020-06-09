cd45dd1dfd82b8a9e37599db1b2519ea031d8c9a
Fernanda Diomede <34989708+fcdiomede@users.noreply.github.com>
2020-06-08 22:11:53 -0500

    Add files via upload

--- snake.py
+++ snake.py
@@ -110,7 +110,12 @@ class snake(object):
         
 
     def reset(self, pos):
-        pass
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
 
     def addCube(self):
         tail = self.body[-1]
@@ -146,7 +151,7 @@ def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
 
     x = 0
-    y=0
+    y = 0
     for l in range(rows):
         x = x + sizeBtwn
         y = y + sizeBtwn
@@ -159,14 +164,14 @@ def redrawWindow(surface):
     global rows, width, s, snack
     surface.fill((0,0,0))
     s.draw(surface)
-    drawGrid(width, rows, surface)
-    pygame. display.update()
+    snack.draw(surface)
+    drawGrid(width,rows, surface)
+    pygame.display.update()
 
 
 def randomSnack(rows, item):
     #get all the positions of the cubes
     positions = item.body
-
     #want to generate random position until we get a valid one
     while True:
         x = random.randrange(rows)
@@ -174,16 +179,23 @@ def randomSnack(rows, item):
         #get a list of a filtered list
         #see if any of the positions are the same as the snake
         #make sure that we don't put a "snack" on top of the snake
-        if len(list(filter(lambda z:z.pos== (x,y), positions))) > 0:
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
             continue
         else:
             break
 
-        return (x,y)
+    return (x,y)
 
 
 def message_box(subject, content):
-    pass
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
 
 
 def main():
@@ -193,10 +205,9 @@ def main():
     win = pygame.display.set_mode((width, width))
     s = snake((255,0,0), (10,10))
     snack = cube(randomSnack(rows, s), color=(0,255,0))
+    flag = True
 
     clock = pygame.time.Clock()
-
-    flag = True
     while flag:
         pygame.time.delay(50)
         #makes sure the game doesn't run more than 10 blocks per second
@@ -206,6 +217,13 @@ def main():
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = cube(randomSnack(rows, s), color=(0,255,0))
+
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print('Score: ', len(s.body))
+                message_box('You Lost!', 'Play again...')
+                s.reset((10,10))
+                break
 
         redrawWindow(win)
 
